f360b8f5cc78d75bcdffa60550e185699a3e46d9
tydmiller <70169338+tydmiller@users.noreply.github.com>
2024-08-28 09:37:50 -0700

    Update STLObject.h
    Added comments

--- STL_Loader/STLObject.h
+++ STL_Loader/STLObject.h
@@ -1,3 +1,8 @@
+/*
+* File: STLObject.h
+* Name: Tyler Miller
+* Description: creates a STL loader object that will load in STL extension files in ASCII and binary.
+*/
 #pragma once
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -12,13 +17,30 @@
 class STLObject
 {
 public:
+	/*Loads a STL file based on path given parameter.
+ 	* Parameters: std::string fileName, the directory that the STL is located
+ 	* inside of the project directory*/
 	void loadSTL(std::string fileName);
+	/*Sets vertex based on current data read in from file
+ 	* Parameter: std::string lineHeader, the current block of data that is currently being red in*/
 	void setVertex(std::string lineHeader);
+	/*Sets normals of the faces based on data read in.
+ 	* Parameter: std::string lineHeader, the current block of data that is currently being red in*/
 	void setNormals(std::string lineHeader);
+	/*Looks to see if vertex has already been read in
+ 	* Parameter: glm::vec3 vertex, the point being search for*/
 	int findVertex(glm::vec3 vertex);
+	/*Gets the completed list of vertices read in from the STL file
+ 	* Return: std::vector<glm::vec3>, the list of vertices from the file*/
 	std::vector<glm::vec3> getListOfVertices();
+	/*Gets the completed list of indices based on the vertices read in from the STL file
+ 	* Return: std::vector<unsigned int>, the list of indices*/
 	std::vector<unsigned int> getListOfIndices();
+	/*Gets the completed list of face normals read in from the STL file
+ 	* Return: std::vector<unsigned int>, the list of face normals*/
 	std::vector<glm::vec3> getlistOfNormals();
+	/*Gets the completed list of face read in from the STL file
+ 	* Return: std::vector<std::vector<glm::vec3>>, the list of vertices triplets*/
 	std::vector<std::vector<glm::vec3>> getListOfFaces();
 
 private:
